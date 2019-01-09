e4e1b7e559f8fcc50a9d544128dc44508682c005
Tyga <b_harrell17@outlook.com>
2019-01-09 16:52:31 -0700

    undefined changes

--- liri.js
+++ liri.js
@@ -12,6 +12,9 @@ var Spotify = require('node-spotify-api');
 var liriCommand = process.argv[2];
 var input = process.argv[3]
 
+// atempt to remove the "undefined" from displaying
+process.stdin.emit('data', "module.exports.repl.ignoreUndefined = true;\n");
+
 function commands(liriCommand, input) {
     switch (liriCommand) {
 
@@ -24,7 +27,7 @@ function commands(liriCommand, input) {
             break;
 
         case "do-what-it-says":
-            getRandom();
+            doWhatItSays();
             break;
 
         //If no command is entered, this is the default message to user
@@ -53,19 +56,20 @@ function getSong(songName) {
     console.log(songName);
 
     //Callback to spotify to search for song name
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
+    spotify.search({ type: 'track', query: songName }, (err, data) => {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        "------------------------------ begin ------------------------------" + "\r\n";
         console.log("Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
             "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url);
 
         //Creates a variable to save text into log.txt file
         var logSong = "Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
             "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url + "\n";
-
+            "------------------------------ end ------------------------------" + "\r\n";
         //Appends text to log.txt file
-        fs.appendFile('log.txt', logSong, function (err) {
+        fs.appendFile('log.txt', logSong, (err) => {
             if (err) throw err;
         });
 
@@ -82,12 +86,12 @@ function getMovie(movieName) {
 
     // Runs a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy";
-
+        
     // Helps debugging
     console.log( `${queryUrl}`);
 
     //Callback to OMDB API to get movie info
-    request(queryUrl, function(error, response, body)  {
+    request(queryUrl, (error, response, body) => {
 
         // If the request is successful
         if (!error && response.statusCode === 200) {
@@ -122,9 +126,9 @@ function getMovie(movieName) {
 };
 
 //Function for Random
-function getRandom() {
+function doWhatItSays() {
     //Reads text in random.txt file
-    fs.readFile("random.txt", "utf8",  function (error, data) {
+    fs.readFile("random.txt", "utf8", (error, data) => {
         if (error) {
             return console.log(error);
         }
